96fdbea0bb36f829f08ef7cf6290e7b22cfe85a9
Levminer <33373714+Levminer@users.noreply.github.com>
2021-03-29 15:37:50 +0200

    Games 3.1.0
    -Fix search
    -SOTTR

--- assets/index.js
+++ assets/index.js
@@ -6,7 +6,7 @@ const search0 = () => {
 	table = document.getElementById("table0")
 	tr = table.getElementsByTagName("tr")
 	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[2]
+		td = tr[i].getElementsByTagName("td")[1]
 		if (td) {
 			txtValue = td.textContent || td.innerText
 			if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -26,7 +26,7 @@ const search1 = () => {
 	table = document.getElementById("table1")
 	tr = table.getElementsByTagName("tr")
 	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[2]
+		td = tr[i].getElementsByTagName("td")[1]
 		if (td) {
 			txtValue = td.textContent || td.innerText
 			if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -43,35 +43,24 @@ const sort = (table, column, asc = false) => {
 	const dirModifier = asc ? 1 : -1
 	const tBody = table.tBodies[0]
 	const rows = Array.from(tBody.querySelectorAll("tr"))
-
-	// Sort each row
 	const sortedRows = rows.sort((a, b) => {
 		const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim()
 		const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim()
-
 		return aColText > bColText ? 1 * dirModifier : -1 * dirModifier
 	})
-
-	// Remove all existing TRs from the table
 	while (tBody.firstChild) {
 		tBody.removeChild(tBody.firstChild)
 	}
-
-	// Re-add the newly sorted rows
 	tBody.append(...sortedRows)
-
-	// Remember how the column is currently sorted
 	table.querySelectorAll("th").forEach((th) => th.classList.remove("th-sort-asc", "th-sort-desc"))
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc)
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc)
 }
-
 document.querySelectorAll(".table-sortable th").forEach((headerCell) => {
 	headerCell.addEventListener("click", () => {
 		const tableElement = headerCell.parentElement.parentElement.parentElement
 		const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell)
 		const currentIsAscending = headerCell.classList.contains("th-sort-asc")
-
 		sort(tableElement, headerIndex, !currentIsAscending)
 	})
 })
